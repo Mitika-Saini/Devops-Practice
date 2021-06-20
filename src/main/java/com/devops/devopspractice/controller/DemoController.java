9a9c592d65bd486e2d79dc0637704a911d5d33b5
Sivaiah Polamreddy <sivaiahp970@gmail.com>
2021-06-20 09:23:55 +0000

    intial setup

---
+++ src/main/java/com/devops/devopspractice/controller/DemoController.java
@@ -0,0 +1,35 @@
+package com.devops.devopspractice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/home")
+public class DemoController {
+	
+	@RequestMapping("/demo")
+	public String testMethod() {
+		
+		return "Welcome to spring boot Demo App, Good know about DevOps.....";
+	}
+	
+	@RequestMapping("/computerMakers")
+	public List<String> ListOfComputerMaker() {
+		
+		List<String> list = new ArrayList<>();
+		list.add("Dell");
+		list.add("Lenono");
+		list.add("HCL");
+		list.add("Samsung");
+		list.add("Thoshiba");
+		list.add("HP");
+		list.add("ASCS");
+		list.add("Apple");
+		
+		return list;
+	}
+	
+}
